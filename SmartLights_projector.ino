83cc936bf599cc0fcc11780ad7d35c166ed3e755
PAVITHRAYANI K <31266604+pavikishoor@users.noreply.github.com>
2020-10-23 13:52:01 +0530

    Create SmartLights_projector.ino

---
+++ SmartLights_projector.ino
@@ -0,0 +1,109 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+  
+#define DEBUG
+#define L1 2   //output pin to control Lamp state
+  
+//WIFI configuration
+const char* ssid = "Mr.manoj";           
+const char* password =  "angrybird";    
+
+const char* mqttServer = "soldier.cloudmqtt.com";  
+const char* mqttUser = "vmcudkes";             
+const char* mqttPassword = "0YK2MLj06TL3";     
+const int mqttPort = 17226;                    
+const char* mqttTopicSub ="home/L1";           
+  
+WiFiClient espClient;
+  
+PubSubClient client(espClient);
+  
+void setup() {
+ Serial.begin(115200);
+ pinMode(L1, OUTPUT);
+ WiFi.begin(ssid, password);
+ while (WiFi.status() != WL_CONNECTED) {
+   delay(500);
+   #ifdef DEBUG
+   Serial.println("Connected to WiFi network..");
+   #endif
+ }
+ #ifdef DEBUG
+ Serial.println("Connecting to a WiFi network");
+ #endif
+ client.setServer(mqttServer, mqttPort);
+ client.setCallback(callback);
+ while (!client.connected()) {
+   #ifdef DEBUG
+   Serial.println("Connecting to MQTT Broker...");
+   #endif
+   if (client.connect("ESP8266Client", mqttUser, mqttPassword )) {
+     #ifdef DEBUG
+     Serial.println("Connected");  
+     #endif
+   } else {
+     #ifdef DEBUG 
+     Serial.print("state failure  ");
+     Serial.print(client.state());
+     #endif
+     delay(2000);
+   }
+ }
+ //Topic
+ client.subscribe(mqttTopicSub);
+}
+  
+void callback(char* topic, byte* payload, unsigned int length) {
+ //stores the received message in a string
+ payload[length] = '\0';
+ String strMSG = String((char*)payload);
+ #ifdef DEBUG
+ Serial.print("The message arrived from the topic: ");
+ Serial.println(topic);
+ Serial.print("message:");
+ Serial.print(strMSG);
+ Serial.println();
+ Serial.println("-----------------------");
+ #endif
+ //set output pin
+ if (strMSG == "1"){       
+    digitalWrite(L1, LOW);  
+ }else if (strMSG == "0"){   
+    digitalWrite(L1, HIGH);  
+ }
+}
+  
+//Function to reconnect MQTT broker
+void reconect() {
+ //while disconnected
+ while (!client.connected()) {
+   #ifdef DEBUG
+   Serial.print("Trying connect to MQTT broker");
+   #endif
+   bool connected = strlen(mqttUser) > 0 ?
+                    client.connect("ESP8266Client", mqttUser, mqttPassword) :
+                    client.connect("ESP8266Client");
+   if(connected) {
+     #ifdef DEBUG
+     Serial.println("Connected!");
+     #endif
+     //subscribe to the topic
+     client.subscribe(mqttTopicSub, 1); 
+   } else {
+     #ifdef DEBUG
+     Serial.println("Failed during connection. Code: ");
+     Serial.println( String(client.state()).c_str());
+     Serial.println("Retrying in 10 sec");
+     #endif
+     //Waiting 10 seconds
+     delay(10000);
+   }
+ }
+}
+  
+void loop() {
+ if (!client.connected()) {
+   reconect();
+ }
+ client.loop();
+}
